e465e38aa7a3bf731bcdc2664a963efd41ee3a67
S-Hun <dinky_apple@naver.com>
2021-12-18 14:08:36 +0900

    Build: Update build options of projects

--- dev-tools/show_version.py
+++ dev-tools/show_version.py
@@ -1,9 +1,15 @@
 import pandas as pd
-import numpy as np
+import urllib.request
+import json
+import re
+import konlpy
 
 def main():
-    print("pandas:", pd.__version__)
-    print("numpy:", np.__version__)
+    print("pandas=="+pd.__version__)
+    print("urllib.requset=="+urllib.request.__version__)
+    print("json=="+json.__version__)
+    print("re=="+re.__version__)
+    print("konlpy=="+konlpy.__version__)
 
 if __name__ == "__main__":
     main()
