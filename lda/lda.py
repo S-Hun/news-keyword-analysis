4a7cea04a701d9bc482ac44d967896b601ef7baf
S-Hun <dinky_apple@naver.com>
2021-12-19 11:36:08 +0900

    Refactor: Edit lda function

--- lda/lda.py
+++ lda/lda.py
@@ -1,56 +1,61 @@
 import pandas as pd
 from konlpy.tag import Okt
+import json
 import gensim
 import glob
 import pyLDAvis
 import pyLDAvis.gensim_models
-# import os
-# os.environ["PYTHONIOENCODING"] = "utf-8"
 
-
-def createLDA(data_path='.'):
-    # csv 파일 읽어들이는 부분  # use your path
+def getFiles(data_path):
     all_files = glob.glob(data_path + "/*.csv")
-
     li = []
-
     for filename in all_files:
         print(filename)
         df = pd.read_csv(filename, encoding='utf-8')
         li.append(df)
+    return li
 
-    df = pd.concat(li, axis=0, ignore_index=True)
+def convTokens(df, columns):
+    data = df[columns[0]]
+    if len(columns) > 1:
+        for column in columns[1:]:
+            temp = [i + j for i, j in zip(data, df[column])]
+            data = temp
+    data_noun_tk = []
 
-    description = df['description']  # df중 description 열만 따로 저장
-    descriotion_noun_tk = []
-    # 파일 읽어들이기 끝
-
-    # 단어 추출
     okt = Okt()
 
-    for d in description:
-        descriotion_noun_tk.append(okt.nouns(d))  # 명사 추출
+    for d in data:
+        data_noun_tk.append(okt.nouns(d))
 
-    descriotion_noun_tk2 = []
+    data_noun_tk2 = []
 
-    for d in descriotion_noun_tk:
-        item = [i for i in d if len(i) > 1]  # 토큰 길이가 1보다 큰거만 추출
-        descriotion_noun_tk2.append(item)
-    # 단어 추출 끝
+    for d in data_noun_tk:
+        item = [i for i in d if len(i) > 1]
+        data_noun_tk2.append(item)
+    
+    return data_noun_tk2
 
-    # lda 모델링
-    dictionary = gensim.corpora.Dictionary(descriotion_noun_tk2)
-    corpus = [dictionary.doc2bow(word) for word in descriotion_noun_tk2]
-    # print(corpus)
+def createLDA(tokens, k):
+    dictionary = gensim.corpora.Dictionary(tokens)
+    corpus = [dictionary.doc2bow(word) for word in tokens]
 
-    k = 3
     lda_model = gensim.models.ldamulticore.LdaMulticore(
         corpus, iterations=12, num_topics=k, id2word=dictionary, passes=1, workers=10)
-    print(lda_model.print_topics(num_topics=k, num_words=15))
-    # lda 모델링 끝
+    with open('LDA_K%s.json' % (k), 'w', encoding='utf8') as outfile:
+                jsonFile = json.dumps(
+                    lda_model.print_topics(num_topics=k, num_words=15), indent=4, ensure_ascii=False)
+                outfile.write(jsonFile)
+                print('LDA_K%s.json' % (k))
 
-    # 시각화
     prepared_data = pyLDAvis.gensim_models.prepare(
         lda_model, corpus, dictionary)
-    pyLDAvis.save_html(prepared_data, 'lda.html')
-    print("lda.html 생성완료")
+    pyLDAvis.save_html(prepared_data, 'LDA_K%s.html' % (k))
+    print("LDA_K%s.html created." % (k))
+
+def init(data_path='.', K=[3]):
+    df = pd.concat(getFiles(data_path), axis=0, ignore_index=True)
+    tokens = convTokens(df, ['title', 'description'])
+    for k in K:
+        createLDA(tokens, k)
+
--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from lib import parsePostData
 from lib import preprocess
 from lda import lda
 
-
 def main():
     path = './data'
     parsePostData.init(data_path=path)
     preprocess.wordCount(data_path=path)
-    lda.createLDA(data_path=path)
+    lda.init(data_path=path, K=[3])
 
 
 if __name__ == "__main__":
